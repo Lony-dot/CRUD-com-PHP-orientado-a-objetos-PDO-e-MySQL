46a219b9a57ff29238ee9e3666caa05d4a56d399
Lony <loonydarck@hotmail.com>
2023-02-22 08:37:10 -0300

    Estilizando a tela de cadastro

---
+++ includes/formulario.php
@@ -0,0 +1,45 @@
+<main>
+
+<section>
+    <a href="index.php">
+        <button class="btn btn-success">Voltar</button>
+    </a>
+</section>
+
+<h2 class="mt-3">Cadastrar Vaga</h2>
+
+<form method="post">
+
+<div class="form-group"> <!--Div para organizar o formulário -->
+    <label>Título</label>
+    <input type="text" class="form-control" name="titulo"> <!-- estiliza o campo -->
+</div>
+
+<div class="form-group"> 
+    <label>Descrição</label>
+    <textarea class="form-control" name="descricao" rows="5"></textarea>
+</div>
+
+<div class="form-group"> 
+    <label>Status</label>
+    
+    <div>   
+        <div class="form-check form-check-inline">
+           <label class="form-control">
+            <input type="radio" name="ativo" value="s"> Ativo
+           </label> 
+        </div>
+
+    
+        <div class="form-check form-check-inline">
+           <label class="form-control">
+            <input type="radio" name="ativo" value="n"> Inativo
+           </label> 
+    </div>
+
+</div>
+
+
+</form>
+
+</main>
