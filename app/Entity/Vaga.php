c102e05fbb033f6fc41d7382abe32334c9a76562
Lony <loonydarck@hotmail.com>
2023-02-25 19:43:34 -0300

    Inclusão de formulário e objetos em cadastrar.php

---
+++ app/Entity/Vaga.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+class Vaga{
+    /**
+     * Identificador único da vaga
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * Título da vaga
+     * @var string
+     */
+    public $titulo;
+
+    /**
+     * Descrição da vaga
+     * @var string
+     */
+    public $descricao;
+
+    /**
+     * Define se a vaga está ativa
+     * @var string(s/n)
+     */
+    public $ativo;
+
+    /**
+     * Data de publicação da vaga
+     * @var string
+     */
+    public $data;
+
+}
